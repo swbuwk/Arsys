877d449ba7bed350d1e84624cf863daf9bd4ed2f
Egor Geberleyn <egeberleyn@gmail.com>
2022-09-24 15:06:20 +0300

    added product dto's

---
+++ src/Arsys.Server/Arsys.API/DTOs/CashDesk/ProductsDto/CreateProductDto.cs
@@ -0,0 +1,34 @@
+﻿using Arsys.API.Application.Common.Mappings;
+using Arsys.API.Application.MediatR.Products.Commands.CreateProduct;
+using Arsys.API.Application.MediatR.Products.Commands.UpdateProduct;
+
+namespace Arsys.API.DTOs.CashDesk.ProductsDto
+{
+    public class CreateProductDto : IMapWith<CreateProductCommand>
+    {
+        public Guid CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateProductDto, CreateProductCommand>()                
+                .ForMember(dto => dto.CategoryId,
+                opt => opt.MapFrom(p => p.CategoryId))
+                .ForMember(dto => dto.Name,
+                opt => opt.MapFrom(p => p.Name))
+                .ForMember(dto => dto.Description,
+                opt => opt.MapFrom(p => p.Description))
+                 .ForMember(dto => dto.ImageUrl,
+                opt => opt.MapFrom(p => p.ImageUrl))
+                  .ForMember(dto => dto.Price,
+                opt => opt.MapFrom(p => p.Price));
+        }
+    }
+}
